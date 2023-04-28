7aef603f9f4ba8081fc31eb77818c621a89ffdea
webcrunch <jarllinduist@gmail.com>
2023-04-28 07:35:59 +0200

    adding more information to the test

--- tests/isHighestCard.test.js
+++ tests/isHighestCard.test.js
@@ -5,11 +5,11 @@ test('check that isHighest returns a higher score for a stronger hand (if two ha
   expect(CompareHands.isHighestCard(new Hand('♦T', '♦3', '♦7', '♠5', '♦K'))).toBeGreaterThan(CompareHands.isHighestCard(new Hand('♣2', '♣4', '♣5', '♣8', '♦9')));
 });
 
-test('check that isHighest returns a higher score for a stronger hand and give less score to the inferier one (if two hands both with nothing but highest card)', () => {
+test('check that isHighest returns a higher score for a stronger hand and give less score to the first hand at this example (if two hands both with nothing but highest card)', () => {
   expect(CompareHands.isHighestCard(new Hand('♣2', '♣4', '♣5', '♣8', '♦9'))).toBeLessThan(CompareHands.isHighestCard(new Hand('♦T', '♦3', '♦7', '♠5', '♦K'))); 
 });
 
-test('check that the value will be falsy if we send in a hand that has more than a highest card ( try a pair', () => {
+test('check that the value will be falsy if we send in a hand that has more than a highest card ( try a pair)', () => {
   let hand = new Hand('♠2', '♦2', '♠4', '♠5', '♠A');
     expect(CompareHands.isHighestCard(hand)).toBeFalsy();
 })
